543abd6a3eda4dea2f883f5d5ec3d2195680b017
jbuberel <jason@buberel.org>
2015-07-19 13:09:10 -0700

    testing pre-commit

--- gometrics.go
+++ gometrics.go
@@ -2,20 +2,20 @@ package main
 
 import (
 	"flag"
+	cls "github.com/jbuberel/gometrics/gerritcls"
+	g "github.com/jbuberel/gometrics/github"
+	iss "github.com/jbuberel/gometrics/gitissues"
 	r "github.com/jbuberel/gometrics/reddit"
 	t "github.com/jbuberel/gometrics/twitter"
-	g "github.com/jbuberel/gometrics/github"
-	cls "github.com/jbuberel/gometrics/gerritcls"
-	iss "github.com/jbuberel/gometrics/gitissues"
 	"log"
-	"time"
 	"os"
 	"strings"
+	"time"
 )
 
 var dirname = flag.String("dir", "/usr/local/google/home/jbuberel/gometrics", "The directory where the file will be written")
-var	since = flag.String("since", time.Now().Add(-47*time.Hour).Format("2006-01-02"), "The start date of the search window, in YYYY-MM-DD format.")
-var	until = flag.String("until", time.Now().Add(-23*time.Hour).Format("2006-01-02"), "The end date of the search window, in YYYY-MM-DD format.")
+var since = flag.String("since", time.Now().Add(-47*time.Hour).Format("2006-01-02"), "The start date of the search window, in YYYY-MM-DD format.")
+var until = flag.String("until", time.Now().Add(-23*time.Hour).Format("2006-01-02"), "The end date of the search window, in YYYY-MM-DD format.")
 
 var clsToggle = flag.Bool("cls", true, "Set to false to disable Gerrit CLs capture")
 var githubToggle = flag.Bool("github", true, "set to false to disable capture of Github stats")
@@ -31,11 +31,8 @@ var githubClientId = ""
 var githubSecretKey = ""
 var githubSecretToken = ""
 
-
-
-
 func init() {
-	log.SetFlags(log.Ldate| log.Ltime | log.Lshortfile)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 	flag.Parse()
 
 	log.Printf("Looking through env vars\n")
@@ -65,20 +62,19 @@ func init() {
 				log.Printf("github_secret_key set from environ to: %v\n", githubSecretToken)
 			}
 		}
-		
+
 	}
-	
-	if len(twitterConsumerSecret)  == 0 || len(twitterConsumerKey) == 0 || len(twitterSecretToken) == 0 {
-		log.Println("Unable to obtain twitter keys from environment variables!!")	
+
+	if len(twitterConsumerSecret) == 0 || len(twitterConsumerKey) == 0 || len(twitterSecretToken) == 0 {
+		log.Println("Unable to obtain twitter keys from environment variables!!")
 		os.Exit(1)
 	}
-	if len(githubClientId)  == 0 || len(githubSecretKey) == 0 || len(githubSecretToken) == 0 {
-		log.Println("Unable to obtain github keys from environment variables!!")	
+	if len(githubClientId) == 0 || len(githubSecretKey) == 0 || len(githubSecretToken) == 0 {
+		log.Println("Unable to obtain github keys from environment variables!!")
 		os.Exit(1)
 	}
 
 }
-
 
 func main() {
 	log.Printf("Saving data to directory: %v\n", *dirname)
