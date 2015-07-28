bb5e36e2a8b81c83d1acd4e344f51cff2f5d00e0
Jason Buberel <jason@buberel.org>
2015-07-28 17:32:26 +0000

    remove stats file

--- mobile/mobile.go
+++ mobile/mobile.go
@@ -3,34 +3,33 @@
 package mobile
 
 import (
-            "fmt"
-        "log"
-        "os"
-        "time"
-        "github.com/PuerkitoBio/goquery"
-        "strings"
-    )
-    
-    var searchUrl = "http://godoc.org/golang.org/x/mobile/app?importers"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
 
-    
+	"github.com/PuerkitoBio/goquery"
+)
+
+var searchUrl = "http://godoc.org/golang.org/x/mobile/app?importers"
+
 func Capture(dirname *string) {
-    doc, err := goquery.NewDocument(searchUrl) 
-    if err != nil {
-        log.Fatal(err)
-    }
+	doc, err := goquery.NewDocument(searchUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    matches := 0
-    doc.Find("div.container table tr>td").Each(func(i int, tr *goquery.Selection) {
-        htmlText, err := tr.First().Html()
-        if err == nil && strings.Contains(htmlText, "/") && !strings.Contains(htmlText, "golang.org/x/mobile") {
-            fmt.Printf("Review %d: %v\n", i, htmlText)
-            matches++
-        }
-    })
-  
-    
-    timestamp := time.Now().Format("2006-01-02")
+	matches := 0
+	doc.Find("div.container table tr>td").Each(func(i int, tr *goquery.Selection) {
+		htmlText, err := tr.First().Html()
+		if err == nil && strings.Contains(htmlText, "/") && !strings.Contains(htmlText, "golang.org/x/mobile") {
+			fmt.Printf("Review %d: %v\n", i, htmlText)
+			matches++
+		}
+	})
+
+	timestamp := time.Now().Format("2006-01-02")
 	outfile := fmt.Sprintf("%v/mobile-imports-%v.csv", *dirname, timestamp)
 	log.Printf("Saving results to file %v\n", outfile)
 	f, err := os.Create(outfile)
@@ -39,6 +38,6 @@ func Capture(dirname *string) {
 	}
 	defer f.Close()
 
-    f.WriteString(fmt.Sprintf("%v,%v\n", timestamp, matches))
+	f.WriteString(fmt.Sprintf("%v,%v\n", timestamp, matches))
 
 }
