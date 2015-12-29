68bc85c948987f8aad8c32eac47471d15fd4d3f6
Jason Buberel <jbuberel@google.com>
2015-12-29 13:11:49 -0800

    fixing recursive author extraction

--- reddit/reddit.go
+++ reddit/reddit.go
@@ -2,71 +2,51 @@ package reddit
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"time"
-	"fmt"
 	"os"
+	"regexp"
+	"time"
 )
 
 type RedditComment struct {
-	Kind    string `json:"kind"`
-	After   string `json:"after"`
-	Before  string `json:"before"`
-	Modhash string `json:"modhash"`
-	Data    struct {
-		Children     []*RedditComment `json:"children"`
-		ApprovedBy   string           `json:"approved_by"`
-		Author       string           `json:"author"`
-		BannedBy     string           `json:"banned_by"`
-		Clicked      bool             `json:"clicked"`
-		Created      float64          `json:"created"`
-		CreatedUtc   float64          `json:"created_utc"`
-		Domain       string           `json:"domain"`
-		Downs        int              `json:"downs"`
-		Gilded       int              `json:"gilded"`
-		ID           string           `json:"id"`
-		Name         string           `json:"name"`
-		NumComments  int              `json:"num_comments"`
-		Permalink    string           `json:"permalink"`
-		Score        int              `json:"score"`
-		Selftext     string           `json:"selftext"`
-		SelftextHTML string           `json:"selftext_html"`
-		Subreddit    string           `json:"subreddit"`
-		SubredditID  string           `json:"subreddit_id"`
-		Thumbnail    string           `json:"thumbnail"`
-		Title        string           `json:"title"`
-		Ups          int              `json:"ups"`
-		URL          string           `json:"url"`
+	Data struct {
+		Children  []*RedditComment `json:"children"`
+		Author    string           `json:"author"`
+		Permalink string           `json:"permalink"`
+		Title     string           `json:"title"`
+		URL       string           `json:"url"`
 	} `json:"data"`
 }
 
 type RedditIndexPage struct {
 	Data struct {
-		Kind     string           `json:"kind"`
-		After    string           `json:"after"`
-		Before   string           `json:"before"`
-		Modhash  string           `json:"modhash"`
 		Children []*RedditComment `json:"children"`
 	} `json:"data"`
 }
 
 type RedditArticlePage []struct {
 	Data struct {
-		Kind     string           `json:"kind"`
-		After    string           `json:"after"`
-		Before   string           `json:"before"`
-		Modhash  string           `json:"modhash"`
 		Children []*RedditComment `json:"children"`
 	} `json:"data"`
 }
 
 type JsonPermalink string
 
+// Retrieve the main HOT listings page
 func GetGolangIndex() []*JsonPermalink {
+	client := &http.Client{}
 
-	resp, err := http.Get("http://www.reddit.com/r/golang/hot.json")
+	req, err := http.NewRequest("GET", "http://www.reddit.com/r/golang/hot.json", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	req.Header.Set("User-Agent", "/u/jbuberel")
+
+	resp, err := client.Do(req)
 	if err != nil {
 		log.Println("Unable to retrieve /r/golang index")
 		return nil
@@ -96,15 +76,28 @@ func GetGolangIndex() []*JsonPermalink {
 
 }
 
-type RedditAuthor string
+// Author The name of the person who wrote the comment
+type Author string
 
-func GetRedditAuthors(permalink JsonPermalink) []*RedditAuthor {
-	var authors = make([]*RedditAuthor, 0)
+// GetRedditAuthors Get the list of authors
+func GetRedditAuthors(permalink JsonPermalink) []Author {
 
-	resp, err := http.Get(string(permalink))
+	log.Printf("  permalink: %v\n", permalink)
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", string(permalink), nil)
 	if err != nil {
 		log.Printf("Unable to retrieve %v\n", permalink)
-		return authors
+		return make([]Author, 0)
+	}
+
+	req.Header.Set("User-Agent", "/u/jbuberel")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("Unable to retrieve /r/golang index")
+		return nil
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
@@ -112,37 +105,32 @@ func GetRedditAuthors(permalink JsonPermalink) []*RedditAuthor {
 	bodyText := string(body[:])
 	log.Println(len(bodyText))
 
-	var articlePage RedditArticlePage
-	err = json.Unmarshal(body, &articlePage)
-	if err != nil {
-		log.Println("error:", err)
-	}
-	for _, article := range articlePage {
-		for _, listing := range article.Data.Children {
-			var author RedditAuthor = RedditAuthor(listing.Data.Author)
-			authors = append(authors, &author)
-			if len(listing.Data.Children) > 0 {
-				sublink := JsonPermalink("http://www.reddit.com" + listing.Data.Permalink[:len(listing.Data.Permalink)-1] + ".json")
-				authors = append(authors, GetRedditAuthors(sublink)...)
-			}
-		}
+	var authors = make([]Author, 0)
+
+	re1, err := regexp.Compile(`\"author\":\s*\"([^\"]+)\"`)
+	result := re1.FindAllStringSubmatch(bodyText, -1)
+
+	for k, v := range result {
+		fmt.Printf("%d. %s\n", k, v[1])
+		authors = append(authors, Author(v[1]))
 	}
 
 	return authors
 
 }
 
+// Capture Start the capture
 func Capture(dirname *string) {
 	log.Println("starting reddit capture")
 	permalinks := GetGolangIndex()
 
-	var authorMap = make(map[RedditAuthor]int)
+	var authorMap = make(map[Author]int)
 	for _, permalink := range permalinks {
 		fmt.Println(*permalink)
 		authors := GetRedditAuthors(*permalink)
 
 		for _, author := range authors {
-			authorMap[*author] += 1
+			authorMap[author]++
 		}
 	}
 
